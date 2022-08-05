47624570f5f0935ea5091ea38a2596842400c49f
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-08-05 11:16:33 +0545

    📝 switchcase in java

---
+++ SwitchStatement.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SwitchStatement {
+
+    public static void SwitchStatement(int no){
+         switch(no){
+            case 1 : System.out.println("Hello");
+            break;
+
+            case 2 : System.out.println("Namaste");
+            break;
+
+            case 3 : System.out.println("Hola");
+            break;
+
+            default : System.out.println("Invalid choice");
+         }
+    }
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your number ");
+        int num = in.nextInt();
+        SwitchStatement(num);
+        in.close();
+    }
+}
