49359ac2ac685aaabfa17108f804452e896c9f2a
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-08-08 07:43:03 +0545

    📝 lowerRight piramid

---
+++ starsFormat/bottomPira.java
@@ -0,0 +1,21 @@
+package starsFormat;
+
+public class bottomPira {
+    public static void main(String args[]){
+        int n = 4;
+
+        //outer loop
+        for(int i=0;i<=n;i++){
+
+            //innerloop-> space print
+            for(int j=0;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //innerloop -> star print
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
