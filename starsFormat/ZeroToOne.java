ef265f67862b41dcd98ddef0b927523f8aaf0a5b
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-08-08 19:25:55 +0545

    🚀 Piramid in 0 to 1 form only

---
+++ starsFormat/ZeroToOne.java
@@ -0,0 +1,22 @@
+package starsFormat;
+
+public class ZeroToOne {
+    public static void main(String args[]){
+        int n = 5;
+        int i,j;
+
+        //outer loop 
+        for( i=1;i<=n;i++){
+            //inner loop
+            for(j=1;j<=i;j++){
+                int sum = i+j;
+                if(sum % 2 ==0){
+                    System.out.print("1 ");
+                }else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
