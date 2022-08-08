9b4fb68e95b9483b77ab566f324742e922ee87d3
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-08-08 07:27:22 +0545

    🚑 kept in Stars package

---
+++ starsFormat/PiraOne.java
@@ -0,0 +1,15 @@
+package starsFormat;
+public class PiraOne {
+ public static void main(String[] args) {
+        int n = 4;
+
+        //outerLoop
+        for(int i=0;i<=n;i++){
+            //InerLoop
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
--- starsFormat/UpperMid.java
+++ starsFormat/UpperMid.java
@@ -1,11 +1,12 @@
-public class PiraOne {
+package starsFormat;
+public class UpperMid {
  public static void main(String[] args) {
         int n = 4;
 
-        //outerLoop
-        for(int i=0;i<=n;i++){
-            //InerLoop
-            for(int j=0;j<i;j++){
+        //outer loop
+        for(int i=n;i>=0;i--){
+            //Inner loop
+            for(int j=0;j<=i;j++){
                 System.out.print("*");
             }
             System.out.println();
