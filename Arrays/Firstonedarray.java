e482b85e02a92e032b439c57e529df4adfa87a79
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-08-13 10:49:40 +0545

    📝 first array program

---
+++ Arrays/Firstonedarray.java
@@ -0,0 +1,36 @@
+package Arrays;
+
+
+
+public class Firstonedarray{
+     public static void main(String[] args) {
+        
+        /*First method  
+        //in this method only the 0th address of data will be shown 
+
+        int[] marks = new  int[3];  //This is the very basic way 
+        marks[0] = 10;
+        marks[1] = 20;
+        marks[2] = 30;
+        System.out.println(marks[0]);*/
+
+         //Second method  
+        //in this method only the 0th address of data will be shown 
+
+        /* 
+       int marks[] = {10,20,30};
+       System.out.println(marks[0]);
+       */
+
+       //Thirs method 
+       //In this method all the values of the array can be displayed 
+       /* 
+       int marks[] = {10,20,30};
+       for(int i=0;i<=2;i++){
+        System.out.println(marks[i]);
+       }*/
+
+       
+       
+    }
+}
