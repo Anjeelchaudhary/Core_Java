81da9ab50cfe62750af7c0bb6fef0cf30e2434ec
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-08-13 19:09:16 +0545

    🐛 first program for two D Array

---
+++ Arrays/TwoDArray/Firstprogram.java
@@ -0,0 +1,10 @@
+package Arrays.TwoDArray;
+
+
+public class Firstprogram {
+    public static void main(String args[]){
+        //Declaration 
+        //int[][] nameOfArray = new int[rows][colums];
+        int[][] number = new int[3][5];
+    }
+}
