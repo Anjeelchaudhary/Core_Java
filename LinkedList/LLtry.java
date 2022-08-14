c3982d39a8c8e5ad8f211b91eda689ad474eb8e1
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-08-14 13:54:48 +0545

    🚑 this is practice program

---
+++ LinkedList/LLtry.java
@@ -0,0 +1,62 @@
+package LinkedList;
+
+public class LLtry {
+    Node head;
+
+    //created a empty node 
+    class Node{
+        String data;
+        Node next;
+        Node(String data){
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+    //adding the node at first in the link 
+    public void addFirst(String data){
+        Node newNode = new Node(data);
+
+        if(head==null){
+           head=newNode;
+        }
+        newNode.next=head;
+        head=newNode;
+    }
+
+    //adding the node at last in the link
+    public void addLast(String data){
+        Node newNode = new Node(data);
+        if(head==null){
+            head = newNode;
+        }
+
+        //here at fist we need to say that head is on currentNode 
+        Node currnNode =head;
+        while(currnNode.next!=null){
+            currnNode=currnNode.next;
+        }
+        currnNode=newNode;
+    }
+
+    //for printing a linklist
+    public void printf(String data){
+    if(head==null){
+        System.out.println("NUll");
+        return;
+    }
+    Node currnNode=head;
+    while(currnNode!=null){
+        System.out.println(" -> ");
+        currnNode=currnNode.next;
+    }
+}
+
+    public static void main(String[] args){
+        LL list = new LL();
+        list.addFirst("a");
+        list.addFirst("is");
+        list.printList();
+    }
+}
