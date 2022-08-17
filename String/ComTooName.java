e72b8c13dd334ab7792928d3ec4f928fedb948f3
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-08-17 18:55:10 +0545

    📝 comparisone of two string

---
+++ String/ComTooName.java
@@ -0,0 +1,14 @@
+package String;
+
+public class ComTooName {
+    public static void main(String[] args){
+        String nameOne = "Tony";
+        String nameTwo = "Tony";
+
+        if(nameOne.compareTo(nameTwo) == 0){
+            System.out.println("The names are equal");
+        }else{
+            System.out.println("The names are not equal");
+        }
+    }
+}
