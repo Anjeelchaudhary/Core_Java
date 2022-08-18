aa9dd54cb70a261311350ca1022ea44c09955f4a
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-08-18 17:11:11 +0545

    ✨ added few more features ie setcharAt

--- String/BuilderString.java
+++ String/BuilderString.java
@@ -7,5 +7,10 @@ public class BuilderString {
 
         //this charAt() helps to give only pointed letter 
         System.out.println(sb.charAt(0));
+
+        //to change the letter
+        //sb.setCharAt(index, ch);<-syntax
+        sb.setCharAt(0, 'B');
+        System.out.println(sb);//in upper line it has changed the value so thats why sb is uesed here
     }
 }
