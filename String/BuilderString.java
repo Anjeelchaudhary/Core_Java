0f5d6a5cd7ca1a46861639c3aa7ae686d38bea6a
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-08-18 17:07:08 +0545

    ✨ charAt() function

---
+++ String/BuilderString.java
@@ -0,0 +1,11 @@
+package String;
+
+public class BuilderString {
+     public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("TomAndJerry");
+        System.out.println(sb);
+
+        //this charAt() helps to give only pointed letter 
+        System.out.println(sb.charAt(0));
+    }
+}
