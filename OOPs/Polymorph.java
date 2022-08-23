a820f2529bcf80c16cf371134b762affc6dcc78d
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-08-23 06:11:54 +0545

    📝 contructures and its type

---
+++ OOPs/Polymorph.java
@@ -0,0 +1,39 @@
+package OOPs;
+
+class cars {
+    String name;
+    String type;
+    String price ;
+
+    public void showInfo(){
+        System.out.println(this.name);
+        System.out.println(this.type);
+        System.out.println(this.price);
+    }
+
+    //copy constructure
+    cars (cars FourWheel) {
+       this.name = FourWheel.name;
+       this.price = FourWheel.price;
+       this.type = FourWheel.type;
+    }
+
+    cars (){
+
+    }
+}
+
+public class Polymorph {
+    public static void main(String[] args) {
+        cars FourWheel = new cars();
+        FourWheel.name = "Benze car";
+        FourWheel.type = "Extra features";
+        FourWheel.price = "One Million";
+        FourWheel.showInfo();
+
+        cars twoWheel = new cars(FourWheel);
+        twoWheel.showInfo(); 
+
+
+    }
+}
