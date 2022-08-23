ed8fe6fd6889540f0bdd18a59f1ab4f5e32d6bee
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-08-23 06:36:07 +0545

    📝 polymorphism in java

---
+++ OOPs/CompileTimePoly.java
@@ -0,0 +1,24 @@
+package OOPs;
+
+class Many{
+    String name;
+    int id;
+
+    public void Show(){
+        System.out.println(this.name);
+    }
+    public void Show(String name){
+        System.out.println(name);
+    }
+}
+
+public class CompileTimePoly {
+    public static void main(String[] args) {
+        Many ncit = new Many();
+        ncit.name = "Tony Strark";
+        ncit.id = 01;
+        ncit.Show();
+        ncit.Show(ncit.name);
+        
+    }
+}
