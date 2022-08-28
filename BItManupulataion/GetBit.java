ff8575b0cd20b40bc7fe3f0acbfab8906fca148a
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-08-28 17:38:37 +0545

    🚑 added comment

--- BItManupulataion/GetBit.java
+++ BItManupulataion/GetBit.java
@@ -2,7 +2,7 @@ package BItManupulataion;
 
 class GetBit {
     public static void main(String[] args) {
-        int number = 5; //0101
+        int number = 5; //0101 <-Binary conversion
         int position = 1;
         int bitmask = 1<<position;
 
