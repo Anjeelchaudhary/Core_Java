d66badfa1f8c0fd51ad34223158fef07f55832bc
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-08-28 17:36:40 +0545

    📝 bitmask problem GetBit

---
+++ BItManupulataion/GetBit.java
@@ -0,0 +1,15 @@
+package BItManupulataion;
+
+class GetBit {
+    public static void main(String[] args) {
+        int number = 5; //0101
+        int position = 1;
+        int bitmask = 1<<position;
+
+        if((bitmask & number) == 0){
+            System.out.println("Zero number was there");
+        }else{
+            System.out.println("One number was there");
+        }
+    }
+}
