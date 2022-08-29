95849c9726cb1b60a6ab4d76201534411e20241f
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-08-29 16:30:14 +0545

    📝 setBit

---
+++ BItManupulataion/SetBit.java
@@ -0,0 +1,12 @@
+package BItManupulataion;
+
+public class SetBit {
+    public static void main(String[] args) {
+        int number = 5;
+    int position = 1;
+    int bitmas = 1 << position;
+
+    int bitBoy = bitmas | number;
+    System.out.println(bitBoy);
+    }
+}
