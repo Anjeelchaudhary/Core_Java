6be7556a56c294c052e912b68b6d9ca57a0328c1
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-08-30 18:26:16 +0545

    ✨ classess and objects

---
+++ classes/FirstProgram.java
@@ -0,0 +1,12 @@
+package classes;
+
+
+public class FirstProgram{
+    int number = 5;
+    public static void main(String[] args) {
+        //class creating
+        FirstProgram FP = new FirstProgram();
+        int result = FP.number;
+        System.out.println(result);
+    }
+}
