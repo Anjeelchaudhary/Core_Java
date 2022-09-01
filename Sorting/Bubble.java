cf39695934b511e02b8e271ef33a25e0e45d9743
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-09-01 17:30:29 +0545

    📝 Bubble sorting

---
+++ Sorting/Bubble.java
@@ -0,0 +1,28 @@
+package Sorting;
+
+public class Bubble {
+    public static void pintArray(int array[]){
+
+        for(int i=0;i<=array.length;i++){
+            System.out.print(array[i]+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int array[] = {6,8,9,5,2,4,7,3,1};
+
+        //Bubble sorting
+
+        for(int i=0;i<=array.length-1;i++){
+            for(int j=0;j<array.length-i-1;j++){
+                if(array[j]>array[j+1]){
+                    //swapping
+                int temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+                }
+            }
+        }
+        pintArray(array);
+    }
+}
