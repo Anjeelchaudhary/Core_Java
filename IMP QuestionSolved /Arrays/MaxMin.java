32b444a9665688beb130c54a69e8c9fb945921b0
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-10-15 11:50:03 +0545

    🎨 Finding maxmin

---
+++ IMP QuestionSolved /Arrays/MaxMin.java
@@ -0,0 +1,12 @@
+public class MaxMin{
+    public static CalcMaxMin(int arrays[],int arraySize){
+
+    }
+    public static void main(String[] args) {
+        int arrays[] = {5,10,22,2,25,150,250};
+        int arraySize = 7;
+        System.out.println("The maximum number is = ");
+        System.out.println("The minimum number is = ");
+        CalcMaxMin(int arrays[],int arraySize);
+    }
+}
