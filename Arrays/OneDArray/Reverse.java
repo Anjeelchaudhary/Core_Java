c9688faa84c73c8e3b904ddd5e7d4ca698e80c1f
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-11-08 18:08:52 +0545

    📝 Reverse a array of number

---
+++ Arrays/OneDArray/Reverse.java
@@ -0,0 +1,30 @@
+package Arrays.OneDArray;
+
+import javax.xml.transform.Templates;
+
+public class Reverse {
+    public static void reverseArray(int numbers[]){
+        int start = 0;
+        int end = numbers.length-1;
+        int temp;
+
+        while(start < end){
+            temp = numbers[start];
+            numbers[start] = numbers[end];
+            numbers[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+   public static void main(String[] args) {
+    int numbers[] = {1,2,3,4,5};
+    reverseArray(numbers);
+
+    for(int i=0;i<numbers.length;i++){
+        System.out.print(numbers[i]+" ");
+    }
+    System.out.println(" ");
+
+   } 
+}
