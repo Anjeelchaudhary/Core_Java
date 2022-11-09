e2e36941b3bd9f73461afedbf6205db4d5d8d648
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-11-09 15:13:32 +0545

    📝 Finding pari in java

---
+++ Arrays/OneDArray/Pair.java
@@ -0,0 +1,18 @@
+package Arrays.OneDArray;
+
+public class Pair {
+    public static void calcPair(int numbers[]) {
+        for(int i=0;i<numbers.length;i++){
+            int currentvalue = numbers[i];
+                for(int j=i+1;j<numbers.length;j++){
+                    int current = numbers[j];
+                    System.out.print("("+currentvalue+","+current+")");
+                    System.out.println();
+                }
+        }
+    }
+    public static void main(String[] args) {
+        int numbers[] = {1,2,3,4,5,6};
+        calcPair(numbers);
+    }
+}
