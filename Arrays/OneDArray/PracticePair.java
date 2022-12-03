8fd9b320605f80f43f3165bc52665698e69f139a
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-12-03 16:42:43 +0545

    Finding pairs practice

---
+++ Arrays/OneDArray/PracticePair.java
@@ -0,0 +1,16 @@
+package Arrays.OneDArray;
+
+public class PracticePair {
+    public static void calcPairs(int arre[]){
+        for(int i=0;i<arre.length;i++){
+            int currentNUmber = arre[i];
+            for(int j=i+1;j<arre.length;j++){
+                System.out.print("("+currentNUmber+","+arre[j]+") ");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arre[] = {1,3,4,5,7,9,10,13};
+        calcPairs(arre);
+    } 
+}
