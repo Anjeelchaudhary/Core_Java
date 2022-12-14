5ab7438d36ac230b0391c649e90d212e720105d3
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-12-15 05:17:35 +0545

    function is added for merge sort

---
+++ 20.Divide and Conquer/MergSort.java
@@ -0,0 +1,16 @@
+class MergSort{
+    public static void calcMergSort(int array[],int si,int ei){
+
+    }
+
+    //here print function is for printing of sorted array
+    public static void printMergSort(int array[]){
+        for(int i=0;i<array.length;i++){
+            System.out.print(array[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int array[] = {6,93,5,2,8};
+    }
+}
