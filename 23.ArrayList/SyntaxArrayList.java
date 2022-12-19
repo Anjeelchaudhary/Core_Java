42ec65ccd0acc28f2d3a463b5a870cd03f34384c
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-12-19 15:52:46 +0545

    Syntax for ArrayList

---
+++ 23.ArrayList/SyntaxArrayList.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList; //ArrayList lies in (java.util) package
+
+public class SyntaxArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(); //this is for integer data
+        ArrayList<String> list2 = new ArrayList<>(); //this is for string values
+        ArrayList<Boolean> list3 = new ArrayList<>(); //this is for the boolean values 
+    }
+}
