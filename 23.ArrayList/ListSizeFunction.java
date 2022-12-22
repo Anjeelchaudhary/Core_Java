47f4ef7088d47c55eca990ca07f967317019026f
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-12-22 07:28:16 +0545

    adding numbers in arraylist and changed added extenion in .vscode/setting

---
+++ 23.ArrayList/ListSizeFunction.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class ListSizeFunction {
+    public static void main(String[] args) {
+        ArrayList<Integer> ls = new ArrayList<>();
+        ls.add(1);
+        ls.add(2);
+        ls.add(3);
+
+        System.out.println(ls.size());
+    }
+}
