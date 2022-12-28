f5d028a2d776027e71aa5787f3ad4cdd7e4596a0
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-12-28 07:33:02 +0545

    ReOccurence program

---
+++ 19.Recursion/Reoccurence.java
@@ -0,0 +1,17 @@
+import java.security.Key;
+
+public class Reoccurence {
+    public static int reOccur(int array[] ,int i,int key) {
+        if(i == array.length-1) {
+            return -1;
+        }
+        if(array[i] == key) {
+            return i;
+        }
+        return reOccur(array, i+1, key);
+    }
+    public static void main(String[] args) {
+        int array[] = {1,2,3,4,5,5,6,7,8,9,10};
+        System.out.println(reOccur(array, 0, 5));
+    }
+}
