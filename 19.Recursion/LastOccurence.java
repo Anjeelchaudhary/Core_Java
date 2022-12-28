ed881bd989552b5afee59215d0730813aeddc69d
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-12-28 07:48:56 +0545

    LastOccurence

---
+++ 19.Recursion/LastOccurence.java
@@ -0,0 +1,17 @@
+public class LastOccurence {
+    public static int occur(int array[],int i ,int key) {
+        if(i == array.length-1) {
+            return -1;
+        }
+        int isFound = occur(array,i+1,key);
+        if(isFound == -1 && array[i]== key ) {
+            return i;
+        }
+        return isFound;
+
+    }
+    public static void main(String[] args) {
+        int array[] = {1,2,3,4,5,5,6,7,8,9,10};
+        System.out.println(occur(array, 0, 5));
+    }
+}
