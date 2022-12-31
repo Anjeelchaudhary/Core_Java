2a6009409e3986c71d658a8e875eb0070f30c0ed
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2022-12-31 16:31:11 +0545

    remove dublicate letter in a string

---
+++ RandomPracticeFile/PracticeDublicate.java
@@ -0,0 +1,20 @@
+public class PracticeDublicate {
+    public static void removeDublicate(String str ,int indx ,StringBuilder newStr , boolean map[]) {
+        if(indx == str.length()) {
+            System.out.println(newStr);
+            return;
+        }
+
+        int currChar = str.charAt(indx);
+        if(map[currChar - 'a'] == true) {
+            removeDublicate(str, indx+1, newStr, map);
+        }else {
+            map[currChar - 'a'] = true;
+            removeDublicate(str, indx+1, newStr.append(currChar), map);
+        }
+    } 
+    public static void main(String[] args) {
+        String str = "AnjeelChaudhary";
+        removeDublicate(str, 0, new StringBuilder(),new boolean[10]);
+    }
+}
