796bed318e6a322fab3e278dffccbffc7894e4e4
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-01-01 12:19:11 +0545

    printing binary string

--- 16.String/StringBuilder.java
+++ 16.String/StringBuilder.java
@@ -1,9 +1,16 @@
 public class StringBuilder {
+    public StringBuilder() {
+    }
+
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
         for(char ch='a';ch<'z';ch++) {
             sb.append(ch);
         }
         System.out.println(sb);
+    }
+
+    public StringBuilder append(String string) {
+        return null;
     }
 }
