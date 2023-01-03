16d37349788b1b9006768e264fbfb7f055a612ea
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-01-03 21:22:33 +0545

    BackTracing on Array

---
+++ 22.Backtracking/BackTrackArr.java
@@ -0,0 +1,24 @@
+public class BackTrackArr {
+    public static void changeArray(int array[],int i,int value) {
+        //base case
+        if(i == array.length) {
+            printTrack(array);
+            return;
+        }
+        // operation
+        array[i] = value;
+        changeArray(array,i+1,value+1);
+        array[i] = array[i]-2;
+    }
+
+    public static void printTrack(int array[]) {
+        for(int i=0;i<array.length;i++) {
+            System.out.print(array[i]+" ");
+        }System.out.println();
+    }
+    public static void main(String[] args) {
+        int array[] = new int[5];
+        changeArray(array, 0, 1);
+        printTrack(array);
+    }
+}
