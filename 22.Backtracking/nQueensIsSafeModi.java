1bfbf59ff54c35fb7e759d33d86b0a7a1b164387
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-01-08 09:06:40 +0545

    fixed some error

--- 22.Backtracking/nQueensIsSafeModi.java
+++ 22.Backtracking/nQueensIsSafeModi.java
@@ -1,42 +1,44 @@
-public class nQueensIsSafe { 
-    public static boolean isSafe(char board[][],int row,int col) {
-        //vertical 
+public class nQueensIsSafeModi {
+    public static boolean isSafe(char board[][],int row ,int col) {
+        //vertical up
         for(int i=row-1;i>=0;i--) {
-            if(board[i][row] == 'Q') {
+            if(board[i][col] == 'Q') {
                 return false;
             }
         }
-        //leftDiago
-        for(int i=row-1,j=col-1; i>=0 && j>=0; i--,j--) {
+        //left
+        for(int i=row-1,j=col-1; i>=0 && j>=0 ; i--,j--) {
             if(board[i][j] == 'Q') {
                 return false;
             }
         }
-        //rightDiago
-        for(int i=row-1,j=col+1;i>=0 && j<=board.length;i--,j++) {
+        //right
+        for(int i=row-1,j=col+1; i>=0 && j>=board.length; i--,j++) {
             if(board[i][j] == 'Q') {
                 return false;
             }
         }
         return true;
+
+
     }
-    public static void nQeen(char board[][],int row) {
-        //base case 
+    public static void nQueens(char board[][],int row) {
+        //base
         if(row == board.length) {
-            print(board);
+            printQueen(board);
             return;
         }
-        //workdone
-        for(int j=0; j<board.length; j++) {
+
+        for(int j=0;j<board.length;j++) {
             if(isSafe(board,row,j)) {
                 board[row][j] = 'Q';
-                nQeen(board,row+1);//function call
-                board[row][j] = 'X';//backtrack
+                nQueens(board,row+1);
+                board[row][j] = 'X';
             }
         }
     }
-    //for printing of borads
-    public static void print(char borad[][]) {
+    public static void printQueen(char borad[][]) {
+        System.out.println("---nQueens---");
         for(int i=0; i<borad.length ; i++) {
             for(int j=0; j<borad.length ; j++) {
                 System.out.print(borad[i][j]+" ");
@@ -45,13 +47,13 @@ public class nQueensIsSafe {
         }
     }
     public static void main(String[] args) {
-        int n = 4;
+        int n= 4;
         char board[][] = new char[n][n];
         for(int i=0;i<n;i++) {
             for(int j=0;j<n;j++) {
                 board[i][j] = 'X';
             }
         }
-        nQeen(board,0);
+        nQueens(board,0);
     }
 }
