6ddceef77af752e8717dcbae1f89b1d33058c8be
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-01-15 15:44:31 +0545

    different types of constructors

--- 18.Oops/Constructor.java
+++ 18.Oops/Constructor.java
@@ -1,13 +1,22 @@
 public class Constructor {
     public static void main(String[] args) {
         Book b1 = new Book();
+        Book b2 = new Book("CreateMe");
+        Book b3 = new Book(250);
+
     }
 }
 class Book{
     String name ;
     int price;
-
+    
     Book() {
         System.out.println("constructor is called ...........");
+    }
+    Book(String name) {
+        this.name = name;
+    }
+    Book(int price) {
+        this.price = price;
     }
 }
