fa55ebc076a105b8bab6a5b30368e0429722ba34
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-01-16 15:31:34 +0545

    First creating a link in between nodes

---
+++ 24.Linklist(part1)/FirstLink.java
@@ -0,0 +1,13 @@
+public class FirstLink {
+    public static class Node {
+        int data;
+        Node next;
+        public Node(int data) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
