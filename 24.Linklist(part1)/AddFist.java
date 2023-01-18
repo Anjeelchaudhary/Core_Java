5979ff0badf9a0947039336fda22a3791e8a4394
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-01-18 12:40:52 +0545

    adding a node at first

---
+++ 24.Linklist(part1)/AddFist.java
@@ -0,0 +1,15 @@
+public class AddFist {
+    public class LinkAddFirst {
+        int data;
+        LinkAddFirst next;
+        void LinkAddFirst(int data) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+    public static LinkAddFirst head;
+    public static LinkAddFirst tail;
+    public static void main(String[] args) {
+        LinkAddFirst ll = new LinkAddFirst();
+    }
+}
