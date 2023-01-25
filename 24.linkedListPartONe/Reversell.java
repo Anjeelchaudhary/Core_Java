6a71159da7ec3b61e0131562e9fbaed40002aafa
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-01-25 21:27:36 +0545

    reverse of ll

---
+++ 24.linkedListPartONe/Reversell.java
@@ -0,0 +1,32 @@
+public class Reversell {
+    public static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int  size;
+
+    public static void revLinkedlist() {
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+
+    }
+    public static void main(String[] args) {
+        LindkedList ll = new LindkedList();
+        ll.revLinkedlist();
+    }
+}
