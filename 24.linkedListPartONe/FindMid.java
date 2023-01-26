197588e8a84fa360b2d5213852137e6db71a9c72
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-01-26 12:13:45 +0545

    mid of the linkedList

---
+++ 24.linkedListPartONe/FindMid.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+
+public class FindMid {
+    public class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public Node middleFind(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow; // here slow is the midNode in linkedList
+    }
+    public static void main(String[] args) {
+    }
+}
