0e3c6f614736d5360ac6a4cae9c0ae4ee5cd1d05
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-01-31 11:48:12 +0545

    first occurence

--- RandomPracticeFile/Recursion.java
+++ RandomPracticeFile/Recursion.java
@@ -31,10 +31,26 @@ public class Recursion {
         }
         return calcSort(array, i+1);
     }
+
+    //FirstOccurence
+    public static int firstOccurence(int array[],int i,int key) {
+        //base case
+        if(i == array.length) {
+            return -1 ;
+        }
+
+        //workdone
+        if(array[i] == key) {
+            return i;
+        }
+
+        return firstOccurence(array, i+1, key);
+    }
     public static void main(String[] args) {
         System.out.println("factorial : "+factorial(5));
         System.out.println("fibbonacy : "+fibbonacy(5));
-        int array[] = {1,2,3,4,6,5};
-        System.out.println(calcSort(array,0));
+        int array1[] = {1,2,3,4,6,5};
+        System.out.println(calcSort(array1,0));
+        System.out.println(firstOccurence(array1, 0, 5));
     }
 }
