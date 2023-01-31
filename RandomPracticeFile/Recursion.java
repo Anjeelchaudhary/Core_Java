233059e56f40ed7addc87d3e6d123ab20849677f
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-01-31 09:44:24 +0545

    checked is array sorted

--- RandomPracticeFile/Recursion.java
+++ RandomPracticeFile/Recursion.java
@@ -18,8 +18,23 @@ public class Recursion {
         int temp =  fibbonacy(number-1) + fibbonacy(number-2);
         return temp;
     }
+
+    //checkIf arraySorted
+    public static boolean calcSort(int array[],int i) {
+        //base case
+        if(i == array.length-1){
+            return true;
+        }
+        //workdone
+        if(array[i] > array[i+1]){
+            return false;
+        }
+        return calcSort(array, i+1);
+    }
     public static void main(String[] args) {
         System.out.println("factorial : "+factorial(5));
         System.out.println("fibbonacy : "+fibbonacy(5));
+        int array[] = {1,2,3,4,6,5};
+        System.out.println(calcSort(array,0));
     }
 }
