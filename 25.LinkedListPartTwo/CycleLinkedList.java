b71a6a6891a3374447cebd2cbc1b7b33ecfdf242
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-02-01 10:04:33 +0545

    cycle finding in linkedList

---
+++ 25.LinkedListPartTwo/CycleLinkedList.java
@@ -0,0 +1,33 @@
+public class CycleLinkedList {
+    //simple creatin a node
+    public class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public static boolean isCycle(int data) {
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = head;
+        System.out.println(isCycle(0));
+    }
+}
