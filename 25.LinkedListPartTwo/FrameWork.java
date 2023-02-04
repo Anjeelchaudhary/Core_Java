a3ab8b32be57e0c5a1e78bae0cc9f2fc178d5861
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-02-04 14:33:44 +0545

    introduced frameWork

---
+++ 25.LinkedListPartTwo/FrameWork.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;;
+
+public class FrameWork {
+    public static void main(String[] args) {
+        //collection frameWork 
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.addLast(2);
+        ll.addFirst(1);
+        ll.addLast(3);
+        System.out.println(ll);
+    }
+}
