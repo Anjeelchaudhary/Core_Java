80e5120b83683a20aee22c3d8454c6fa283623c7
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-02-06 08:39:57 +0545

    node class

---
+++ 25.LinkedListPartTwo/MergInList.java
@@ -0,0 +1,24 @@
+class MergInList {
+    //created a node class 
+    public class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    
+    public static void main(String[] args) {
+        // MergInList ll = new MergInList();
+        // ll.addFirst(1);
+        // ll.addFirst(2);
+        // ll.addFirst(3);
+
+        // ll.head = ll.Merg(ll.head);
+        // ll.print();
+    }
+}
