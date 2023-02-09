ebc4626a6d05fb7919850382f9fff564bccd7685
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-02-09 18:08:38 +0545

    practice

---
+++ RandomPracticeFile/practiceZigzag.java
@@ -0,0 +1,30 @@
+public class practiceZigzag {
+    public class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public void zigZagZone() {
+        //find mid
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+
+        //reverse 2nd half
+        
+        //alterMerg - zigzag
+    }
+    public static void main(String[] args) {
+        
+    }
+}
