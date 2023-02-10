593f78882236311195b06ed4011ac1d60dec9ffa
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-02-10 20:07:37 +0545

    bitmanupulataion

---
+++ RandomPracticeFile/FastExpontialPractice.java
@@ -0,0 +1,16 @@
+public class FastExpontialPractice {
+    public static int FastExp(int pow,int number) {
+        int ans = 1;
+        while(number>0) {
+            if((number&1)!=0) {
+                ans = ans*pow;
+            }
+            pow = pow * pow;
+            number= number>>1;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        System.out.println(FastExp(5,2));
+    }
+}
