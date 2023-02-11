23037904cf68112bb7e0a7b29135fa65e292bc31
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-02-11 17:17:05 +0545

    reversing and adding random nodes

--- RandomPracticeFile/practiceZigzag.java
+++ RandomPracticeFile/practiceZigzag.java
@@ -20,9 +20,32 @@ public class practiceZigzag {
         }
         Node mid = slow;
 
-        //reverse 2nd half
+        //reverse 2nd half(3-variable and 4-steps)
+        Node current = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+        while(current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        Node rightHead = current;
+        Node leftHead = head;
+        Node nextR,nextL;
         
         //alterMerg - zigzag
+        while(rightHead != null && leftHead != null) {
+            nextL = leftHead.next;
+            leftHead.next = rightHead;
+            nextR = rightHead.next;
+            rightHead.next = nextL;
+
+            rightHead = rightHead.next;
+            leftHead = leftHead.next;
+        }
     }
     public static void main(String[] args) {
         
