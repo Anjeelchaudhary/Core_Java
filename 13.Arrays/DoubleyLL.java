217e6a20c04236a83140ffdf0596dec8a7e42647
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-02-12 08:01:54 +0545

    creation of doubly linkedLIst

---
+++ 13.Arrays/DoubleyLL.java
@@ -0,0 +1,19 @@
+public class DoubleyLL {
+
+    //Crationg of doubly linkedList
+    public class Node {
+        int data;
+        Node next;
+        Node prev;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static void main(String[] args) {
+        
+    }
+}
