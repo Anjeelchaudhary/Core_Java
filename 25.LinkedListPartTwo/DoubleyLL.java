102f2ab2268cccd6313343b880e747f87dd9a44a
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-02-12 08:28:45 +0545

    adding at first in doubley LinkedList

--- 25.LinkedListPartTwo/DoubleyLL.java
+++ 25.LinkedListPartTwo/DoubleyLL.java
@@ -13,7 +13,26 @@ public class DoubleyLL {
     }
     public static Node head;
     public static Node tail;
+
+    //adding at first
+    public void firstAdd(int data) {
+        Node newNode = new Node(data);
+
+        //base case
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        //workdone
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
     public static void main(String[] args) {
-        
+        DoubleyLL ll = new DoubleyLL();
+        ll.firstAdd(2);
+        ll.firstAdd(1);
+        ll.firstAdd(0);
     }
 }
