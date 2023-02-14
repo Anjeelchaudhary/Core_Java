62d1ae304846be41badc62624a28a0b7c0a42967
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-02-14 09:18:06 +0545

    getting error while using .append()

--- 16.String/StringBuilder.java
+++ 16.String/StringBuilder.java
@@ -1,16 +1,12 @@
 public class StringBuilder {
-    public StringBuilder() {
-    }
-
     public static void main(String[] args) {
+        // StringBuilder sb = new StringBuilder();
+        // // for(char ch='a';ch<'z';ch++) {
+        // //     sb.append(ch);
+        // // }
         StringBuilder sb = new StringBuilder();
-        for(char ch='a';ch<'z';ch++) {
-            sb.append(ch);
-        }
+        char ch = 'a';
+        sb.append(ch);
         System.out.println(sb);
-    }
-
-    public StringBuilder append(String string) {
-        return null;
     }
 }
