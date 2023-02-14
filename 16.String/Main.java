50f8da262b8b42524e19322f49ffc64410b34e23
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-02-14 20:24:57 +0545

    LeetCode's sollution

---
+++ 16.String/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+class Main {
+        public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append('a');
+		System.out.println(sb);
+	}
+}
