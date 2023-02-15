8bfaf82f55eddb277b136ec2e421e6f01b4bd0bb
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-02-15 16:54:28 +0545

    adding of array and integer

---
+++ LeetCode/Daily/ArrayToInteger.java
@@ -0,0 +1,33 @@
+package Daily;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayToInteger {
+    public static void toIntegerSum(int num[],int k) {
+        //Converting array into integer form
+        int result = 0;
+        for(int i=0;i<=num.length-1;i++) {
+            result = result * 10 ;
+            result = result + num[i];
+        }
+
+        //Now adding inter form and k'th value
+        result = result + k;
+        ArrayList<Integer> al = new ArrayList<>();
+        while(result != 0) {
+            int finalResult = result % 10;
+            result = result / 10;
+            al.add(finalResult);
+        }
+        Collections.reverse(al);
+        System.out.println(al);
+    }
+    
+
+    public static void main(String[] args) {
+        int num[] = {1,2,0,0};
+        int k = 34;
+        toIntegerSum(num, k);
+    }
+}
