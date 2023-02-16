4fdfa87eb91fcd4d83e48eb68aa54d81eb91fea6
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-02-16 14:57:07 +0545

    stack implementation

---
+++ 26.Stack/Stack1.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class Stack1 {
+    static class Stack {
+        static ArrayList<Integer> al = new ArrayList<>();
+
+        //This function only says if the arraylist is empty or not 
+        static boolean isEmpty() {
+            return al.size() == 0;
+        }
+
+        //for adding data in arraylist
+        public static void push(int data) {
+            al.add(data);
+        }
+
+        //for popping out :Note-(Here return type of pop is int because
+        // before doing pop we have to return stored data from stack )
+        public static int pop() {
+            int top = al.get(al.size()-1);      //1st step : taking out top object
+            al.remove(al.size()-1);             // removing top  object
+            return top;                         //returning top
+        }
+
+        //peek
+        public static int peek() {
+            return al.get(al.size()-1);
+        }
+    }
+    public static void main(String[] args) {
+        Stack stak = new Stack();
+        stak.push(1);
+        stak.push(2);
+        while(!stak.isEmpty()) {
+            System.out.println( stak.peek());
+            stak.pop();
+        }
+
+    }
+}
