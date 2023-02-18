d8ad314a6c0c10abff7576696a3d427b2309736c
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-02-18 14:20:07 +0545

    finnally completed stack using linkedlist

--- 26.Stack/StackWidLL.java
+++ 26.Stack/StackWidLL.java
@@ -27,8 +27,32 @@ public class StackWidLL {
             head = newNode;
 
         }
+
+        public static int pop() {
+            if(isEmpty()) {
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        public static int peek() {
+            if(isEmpty()) {
+                return -1;
+            }
+            return head.data;
+        }
     }
     public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3); 
+        while(!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
         
     }
 }
