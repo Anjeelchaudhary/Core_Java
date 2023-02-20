7dee5cdaa2a7868a48c69156122f3f9be95b9149
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-02-20 18:50:02 +0545

    commented

--- 26.Stack/ReverseString.java
+++ 26.Stack/ReverseString.java
@@ -2,19 +2,20 @@ import java.util.Stack;
 
 public class ReverseString {
     public static String isReverse(String str) {
+        //first case where given string is pushed into stack
         Stack<Character> s = new Stack<>();
         int indx = 0;
         while(indx < str.length()) {
             s.push(str.charAt(indx));
             indx++;
         }
-
+        //poped data from stack is stored by appending in StringBuffer
         StringBuffer sb = new StringBuffer();
         while(!s.isEmpty()) {
             char res = s.pop();
             sb.append(res);
         }
-        return sb.toString();
+        return sb.toString();  //sb is in char form so toString() method is used to convert it into string form
     }
     public static void main(String[] args) {
         String quest = "abc";
