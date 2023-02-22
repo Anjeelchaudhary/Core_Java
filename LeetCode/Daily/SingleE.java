b3a3b5300119bad17c18d04f29c228a03a009dd7
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-02-22 17:37:53 +0545

    cycle in ll

--- LeetCode/Daily/SingleE.java
+++ LeetCode/Daily/SingleE.java
@@ -6,9 +6,9 @@ public class SingleE {
         int i = 0;
         int length = nums.length-1;
         //base case
-        if(length==0 || length==1) {
-            return nums[i];
-        }
+        // if(length==0 || length==1) {
+        //     return nums[i];
+        // }
 
         // workdone
         while(i <= length) {
@@ -25,8 +25,8 @@ public class SingleE {
     public static void main(String[] args) {
         // int nums[] = {1,1,2,3,3,4,4,8,8};
         // int nums[] = {3,3,7,7,10,11,11};
-        // int nums[] = {1};
-        int nums[] = {1,1,2};
+        int nums[] = {};
+        // int nums[] = {1,1,2};
         int ouput = singleNonDuplicate(nums);
         System.out.println(ouput);
         
---
+++ RandomPracticeFile/PracticeCycle.java
@@ -0,0 +1,34 @@
+public class PracticeCycle {
+    //simple creatin a node
+    public class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public static boolean isCycle(int data) {
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        CycleLinkedList ll = new CycleLinkedList();
+        head = ll.new Node(1);
+        head.next = ll.new Node(2);
+        head.next.next = ll.new Node(3);
+        head.next.next.next = head;
+        System.out.println(isCycle(0));
+    }
+}
