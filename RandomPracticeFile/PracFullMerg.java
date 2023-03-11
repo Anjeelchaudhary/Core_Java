44d125dd78e76940a4d64c52776f7ab83f8828e0
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-03-11 18:52:13 +0545

    print function added

--- RandomPracticeFile/PracFullMerg.java
+++ RandomPracticeFile/PracFullMerg.java
@@ -39,7 +39,13 @@ public class PracFullMerg {
             array[i] = temppo[k];
         }
     }
+
+    public static void print(int array[]) {
+        for(int i=0;i<array.length;i++) {
+            System.out.print(array[i]+" ");
+        }System.out.println();
+    }
     public static void main(String[] args) {
-        
+        int array[] = {6,3,9,5,2,8};
     }
 }
