c048b58583386c05c40e72af8c3ac51fafb356be
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-03-12 21:23:57 +0545

    Finding Maximum and minimum

---
+++ RandomPracticeFile/AlgoForExam/MaxMin.java
@@ -0,0 +1,26 @@
+package AlgoForExam;
+
+public class MaxMin {
+
+    public static  void calcMaxMin(int array[]) {
+      int max = 0;
+      int min = 0;
+      
+      for(int i=0;i<array.length;i++) {
+        if(array[i] > max) {
+            max = array[i];
+        }
+        if(array[i] < min) {
+            min = array[i];
+        }
+      }
+
+      System.out.println("Max value is : "+max);
+      System.out.println("Min value is : "+min);
+
+    }
+    public static void main(String[] args) {
+        int array[] = {500,200,85,1,0,399,750};
+        calcMaxMin(array);
+    }
+}
