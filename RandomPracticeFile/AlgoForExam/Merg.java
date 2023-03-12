f59bbfeb6bc42d1645003cb13531500ef866d10b
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-03-12 19:37:43 +0545

    CalculationPart is added

--- RandomPracticeFile/AlgoForExam/Merg.java
+++ RandomPracticeFile/AlgoForExam/Merg.java
@@ -1,5 +1,23 @@
 package AlgoForExam;
 
 public class Merg {
-    
+    public static void calculation(int array[],int startI ,int endingI) {
+        //base case
+        if(startI >= endingI) {
+            return;
+        }
+        
+        int mid =startI + (endingI - startI) / 2;  //finding Mid
+        calculation(array, startI, mid);           //sorting for left
+        calculation(array, mid+1, endingI);        //sorting for right
+        calcMerg(array , startI , mid , endingI);  //actural sorting
+    }
+
+    public static void calcMerg(int array[] ,int startI ,int mid ,int endingI) {
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
