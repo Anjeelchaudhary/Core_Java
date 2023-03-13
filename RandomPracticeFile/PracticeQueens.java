b5e03c5a40692d4641a690af26b0307d5f5a4f84
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-03-13 21:47:25 +0545

    placed Queens is printed

--- RandomPracticeFile/PracticeQueens.java
+++ RandomPracticeFile/PracticeQueens.java
@@ -1,10 +1,25 @@
 public class PracticeQueens {
 
     public static void queenS(char board[][], int row) {
+        
+        if(row == board.length) {
+            printQueen(board);
+            return;
+        }
+
         for(int j = 0; j < board.length ;j++) {
             board[row][j] = 'Q';    //placed int board
             queenS(board, row+1);   //recursive call
             board[row][j] = '-';    //When it returns using bracktrack it will clear occupied place by Queen
+        }
+    }
+
+    public static void printQueen(char board[][]) {
+        for(int i=0;i<board.length;i++) {
+            for(int j=0;j<board.length;j++) {
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
         }
     }
     public static void main(String[] args) {
