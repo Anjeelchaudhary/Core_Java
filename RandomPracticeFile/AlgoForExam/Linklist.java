613b44fbda7a9f59e5238703f9d0f6f37b9e2eb2
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-03-14 19:06:51 +0545

    adding Node at first & last and print function

--- RandomPracticeFile/AlgoForExam/Linklist.java
+++ RandomPracticeFile/AlgoForExam/Linklist.java
@@ -19,7 +19,7 @@ public class Linklist {
         //adding at first
         //base case
         if(head == null) {
-            System.out.println("empty is empty ");
+            System.out.println("It was empty");
             head = tail = newNode;
             return;
         }
@@ -28,7 +28,43 @@ public class Linklist {
         newNode.next = head;
         head = newNode;
     }
+
+    public void addNodeLast(int data) {
+        Node newNode = new Node(data);
+
+        if(head == null) {
+            System.out.println("It was empty");
+            head = tail = newNode;
+            return;
+        }
+
+        //if there was node Already there
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    //for printingFunction
+    public void printMe() {
+        //base case
+        if(head == null) {
+            System.out.println("Empty Node");
+            return;
+        }
+
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.data+" -> ");
+            temp = temp.next;
+        }
+
+    }
     public static void main(String[] args) {
-        
+        Linklist ll = new Linklist();
+        ll.addNodeFirst(3);
+        ll.addNodeFirst(2);
+        ll.addNodeFirst(1);
+        ll.addNodeLast(4);
+        ll.addNodeLast(5);
+        ll.printMe();
     }
 }
