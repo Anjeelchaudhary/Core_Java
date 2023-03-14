2d30bdde092debe6c23bd65a4f087d776202dede
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-03-14 12:21:57 +0545

    algorithm for stack input

---
+++ RandomPracticeFile/AlgoForExam/AlgoStack.java
@@ -0,0 +1,23 @@
+package AlgoForExam;
+public class AlgoStack {
+
+    public static void stAlgorithm() {
+
+        //This is for push operation in stack 
+        //base case
+        if(top >= num) {
+            System.out.println("Stack is full");
+            return;
+        }
+
+        //if stack is not full
+        top = top + 1;;
+        Stack[top] = Data;
+        Stop
+
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
