3857daf5802a84bf398e1effeee621cb3aa9d4c2
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-03-14 12:31:10 +0545

    adding element in queue

---
+++ RandomPracticeFile/AlgoForExam/AlgoQueue.java
@@ -0,0 +1,26 @@
+package AlgoForExam;
+
+import javax.xml.crypto.Data;
+
+public class AlgoQueue {
+
+    public static void queAlgorithm(int queue[], int num) {
+
+        //enqueue or inserting an element in queue
+        //base case 
+        if(head >= num-1) {
+            System.out.println("Queue is full");
+            return;
+        }
+
+        //if queue is not full
+        head = head + 1 ;
+        queue[head] = Data;
+        Stop
+
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
