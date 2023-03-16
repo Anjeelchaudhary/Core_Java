737485ab9f286b6e82b5fdd1687ef3fa8a8b7553
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-03-16 20:09:22 +0545

    algorithm for inserting and deleting in stack

--- RandomPracticeFile/AlgoForExam/Stack.java
+++ RandomPracticeFile/AlgoForExam/Stack.java
@@ -1,5 +1,32 @@
 package AlgoForExam;
 
 public class Stack {
-    
+
+    public static void stPushPop() {
+
+        //for push operation in stack
+        if(top == N-1) {
+            display("Stack is full");
+            return;
+        }
+
+        top = top +1;
+        Stack[top] = data;
+        Stop;
+
+
+
+        //for pop operatioon in stack
+        if(top == null) {
+            display("Stack is empty");
+            return;
+        }
+
+        int value = Stack[data];
+        int top = top - 1;
+        Stop;
+    }
+    public static void main(String[] args) {
+        
+    }
 }
