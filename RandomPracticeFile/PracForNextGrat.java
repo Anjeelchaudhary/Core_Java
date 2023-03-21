248f9b9b0574bc30c46980e7a80feb3c5b5abd13
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-03-21 17:02:50 +0545

    blueprint is added

---
+++ RandomPracticeFile/PracForNextGrat.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class PracForNextGrat {
+
+    public static void calcMyGreater(int array[]) {
+        Stack<Integer> stk = new Stack<>();
+        int arr1[] = new int[array.length];
+
+        for(int i=array.length;i>0;i--) {
+        }
+    }
+    public static void main(String[] args) {
+        int array[] = {6 , 8 , 0 , 1 , 3};
+
+    }
+}
