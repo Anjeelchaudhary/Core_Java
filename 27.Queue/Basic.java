1814a4adca37288f943901eea1dadda23532f754
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-03-25 09:32:29 +0545

    emptyness function is addeed

--- 27.Queue/Basic.java
+++ 27.Queue/Basic.java
@@ -1,4 +1,22 @@
 public class Basic {
+
+    static class Queue {
+        static int arra[];
+        static int size;
+        static int rear;
+
+        //construture
+        Queue(int n) {
+            arra = new int [n];
+            size = n;
+            rear = -1;
+        }
+
+        //checking array if array is empty or not
+        public static boolean isEmpty() {
+            return rear == -1;
+        }
+    }
     public static void main(String[] args) {
         
     }
