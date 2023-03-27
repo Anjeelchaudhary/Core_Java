3f4bffecc77a8734c3033ff2504aa8918bc8e694
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-03-27 10:57:36 +0545

    adding function added

--- 27.Queue/CirQueue.java
+++ 27.Queue/CirQueue.java
@@ -13,7 +13,29 @@ public class CirQueue {
             rear = -1;
             front = -1;
         }
-        
+
+        public static boolean isFulll() {
+            //here if size = 3 and where array index is 0 , 1 , 2 and rear is at 2 then (rear+1)%size = 0(here 0 is remainder)
+            //rear is at front of front and if rear wants to jump for next step it will found front 
+            return (rear+1)%size == front;        
+        }
+
+
+        //adding function in circular Queue
+        public static void queAdd(int data) {
+            if(isFulll()) {
+                System.out.println("Circular Queue is full ");
+                return;
+            }
+
+            //adding element in empty array
+            if(front == -1) {
+                front = 0;
+            }
+            //here if size = 3 and where array index is 0 , 1 , 2 and rear is at 2 then (rear+1)%size = 0(here 0 is remainder)
+            rear = (rear+1)%size;  //updating
+            arr[rear] = data;
+        }
     }
     public static void main(String[] args) {
         
