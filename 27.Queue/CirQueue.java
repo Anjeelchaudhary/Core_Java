f1e3d2152c975916d9560906916065daa2f32c68
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-03-27 10:37:08 +0545

    Circular queue in  O(1)

---
+++ 27.Queue/CirQueue.java
@@ -0,0 +1,21 @@
+public class CirQueue {
+
+    //new class for Queue
+    public static class Queue {
+        static int arr[];
+        static int size;
+        static int rear;
+        static int front;
+        //Constructure
+        Queue(int n) {
+            arr = new int[n];
+            size = n;
+            rear = -1;
+            front = -1;
+        }
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
