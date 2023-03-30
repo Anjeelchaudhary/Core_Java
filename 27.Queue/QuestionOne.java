f8d153885a49942a61f4ec96cb203798c2609db2
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-03-30 08:55:59 +0545

    function for emptiness in queue

--- 27.Queue/QuestionOne.java
+++ 27.Queue/QuestionOne.java
@@ -1,4 +1,27 @@
 public class QuestionOne {
+
+    //Class for node
+    public static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    //class for Queue
+    public static class Queue {
+        static Node head = null;
+        static Node tail = null;
+
+        //if queue is empty 
+        public static boolean isEEmmpptty() {
+            return head == null && tail == null;
+        }
+
+        
+    }
     public static void main(String[] args) {
         
     }
