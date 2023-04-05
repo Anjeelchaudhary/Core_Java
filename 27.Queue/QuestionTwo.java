26c0c666671a5c8536f10b7d1064080df0e164ef
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-04-05 19:47:10 +0545

    Queue implemented using two stacks

--- 27.Queue/QuestionTwo.java
+++ 27.Queue/QuestionTwo.java
@@ -11,14 +11,54 @@ public class QuestionTwo {
             return st1.isEmpty();
         }
 
+        //adding an element
         public static void adding(int data) {
             //here until stack1 is not empty, simply we pop an element and store it into stack2 
             while(!st1.isEmpty()) {
                 st2.push(st1.pop());
             }
+
+            //2nd step
+            st1.push(data);
+
+            //3rd step
+            while(!st2.isEmpty()) {
+            //here until stack2 is not empty, simply we pop an element and store it into stack1 
+                st1.push(st2.pop());
+            }
         }
+
+        //removing an element to form a queue
+        public static int removing() {
+            //base case
+            if(st1.isEmpty()) {
+                System.out.println("Queue is empty ");
+                return -1;
+            }
+
+            //at last remove all element from stack1
+            return st1.pop();
+       }
+
+       public static int peeking() {
+        //base case
+        if(st1.isEmpty()) {
+            System.out.println("Queue is empty ");
+            return -1;
+        }
+
+        //at last remove all element from stack1
+        return st1.peek();
+       }
     }
     public static void main(String[] args) {
-        
+        Queue q = new Queue();
+        q.adding(1);
+        q.adding(2);
+        q.adding(3);
+        while(!q.isEmpty()) {
+            System.out.println(q.peeking());
+            q.removing();
+        }
     }
 }
