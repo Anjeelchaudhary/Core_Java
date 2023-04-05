d32a2d2e2065a858cd009a838d88269938c3fbab
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-04-05 19:17:21 +0545

    two stack added

--- 27.Queue/QuestionTwo.java
+++ 27.Queue/QuestionTwo.java
@@ -1,4 +1,12 @@
+import java.util.*;;
+
 public class QuestionTwo {
+
+    //new class
+    public class Queue {
+        Stack<Integer> st1 = new Stack<>();
+        Stack<Integer> st2 = new Stack<>();
+    }
     public static void main(String[] args) {
         
     }
