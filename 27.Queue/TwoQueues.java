f24e87e6d6bea4a0abd13628946a77915b2b6aa3
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-04-06 19:40:29 +0545

    blueprint

---
+++ 27.Queue/TwoQueues.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class TwoQueues {
+    public class Stack {
+        static Queue<Integer> q1 = new LindkedList();
+    }
+    public static void main(String[] args) {
+        
+    }
+}
