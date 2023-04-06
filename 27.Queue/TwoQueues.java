9875461cd0d226ff207384fed9a968df98405d49
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-04-06 19:44:01 +0545

    someErros

--- 27.Queue/TwoQueues.java
+++ 27.Queue/TwoQueues.java
@@ -1,8 +1,14 @@
 import java.util.*;
+import java.util.LinkedList;
 
 public class TwoQueues {
     public class Stack {
-        static Queue<Integer> q1 = new LindkedList();
+        static Queue<Integer> q1 = new LinkedList<>();
+        static Queue<Integer> q2 = new LinkedList<>();
+
+        public static boolean isEmpty() {
+            return q1.isEmpty();
+        }
     }
     public static void main(String[] args) {
         
