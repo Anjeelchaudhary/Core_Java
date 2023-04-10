179726c607495dbd034de8e377d4adc70ae892c3
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-04-10 20:08:16 +0545

    incomplete Code

--- 27.Queue/QuestionThree.java
+++ 27.Queue/QuestionThree.java
@@ -1,3 +1,14 @@
+import java.util.*;
+import java.util.LinkedList;
+
 public class QuestionThree {
-    
+
+    public static void printFirtNon(String st) {
+        int freq[] = new [26];
+        Queue<Character> q = new LinkedList<>();
+        
+    }
+    public static void main(String[] args) {
+        String st = "aabccxb";
+    }
 }
