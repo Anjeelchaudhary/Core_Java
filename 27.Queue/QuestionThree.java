329fbad9306bcb70c5e7810edbd31d72f2bc10fa
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-04-11 19:48:02 +0545

    completed

--- 27.Queue/QuestionThree.java
+++ 27.Queue/QuestionThree.java
@@ -11,9 +11,21 @@ public class QuestionThree {
             char ch = st.charAt(i);
             q.add(ch);
             freq[ch-'a']++;
+
+            while(!q.isEmpty() && freq[q.peek()-'a'] > 1) {
+                q.remove();
+            }
+
+            if(q.isEmpty()) {
+                System.out.print(-1+" ");
+            }else {
+                System.out.print(q.peek()+" ");
+            }
         }
+        System.out.println();
     }
     public static void main(String[] args) {
         String st = "aabccxb";
+        printFirtNon(st);
     }
 }
