0be71cb74d18dcda784df4236e73172d8626414e
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-04-11 19:44:02 +0545

    fixed error

--- 27.Queue/QuestionThree.java
+++ 27.Queue/QuestionThree.java
@@ -4,9 +4,14 @@ import java.util.LinkedList;
 public class QuestionThree {
 
     public static void printFirtNon(String st) {
-        int freq[] = new [26];
+        int freq[] = new int[26];
         Queue<Character> q = new LinkedList<>();
-        
+
+        for(int i=0;i<st.length();i++) {
+            char ch = st.charAt(i);
+            q.add(ch);
+            freq[ch-'a']++;
+        }
     }
     public static void main(String[] args) {
         String st = "aabccxb";
