761661e23a42abdd6c5e11c1d0421954ad82ad46
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-04-12 18:43:29 +0545

    Node class added

--- 29.BinaryTree PatOne/BinaryTree.java
+++ 29.BinaryTree PatOne/BinaryTree.java
@@ -1,4 +1,16 @@
 public class BinaryTree {
+
+    //Node Class
+    public class Node {
+        int data;
+        Node left;
+        Node right;
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
     public static void main(String[] args) {
         
     }
