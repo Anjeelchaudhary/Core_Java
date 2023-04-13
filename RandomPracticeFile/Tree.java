9d63aba4a3e3e80f88bcb89112f46f24eace2a2d
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-04-13 19:28:49 +0545

    TreeBinary

---
+++ RandomPracticeFile/Tree.java
@@ -0,0 +1,21 @@
+public class Tree {
+
+    //created a Node
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+        Node(int data) {
+            this.data = data;
+            this.right = null;
+            this.left = null;
+        }
+    }
+
+    static class BTrees {
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
