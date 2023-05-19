b1f9b77767718f6cdf63ffbca267b570cc958af8
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-05-19 20:02:20 +0545

    linkedlist

---
+++ RandomPracticeFile/Linked.java
@@ -0,0 +1,58 @@
+public class Linked {
+
+    //making a node
+    public static class Node {
+        int data;
+        Node next;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;//assignig head
+    public static Node tail;//assigning tail
+    
+    //addFirst function
+    public static void addFirst(int data) {
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+    //addLast function
+    public static void addLast(int data) {
+        Node newNode = new Node(data);
+
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+    //Topringt function
+    public static void print() {
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.data+" -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        LindkedList ll = new LindkedList(); //printlist
+        ll.addFirst(2);
+        ll.print();
+        ll.addFirst(1);
+        ll.print();
+        ll.addLast(3);
+        ll.print();
+        ll.addLast(4);
+        ll.print();
+
+    }
+}
